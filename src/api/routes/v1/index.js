f9e89c6b031eaae5ad7faab5876f1f61e6946534
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2024-05-11 23:54:15 +0530

    deploy

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,15 +1,9 @@
 import express from 'express';
 import userRoutes from './user.route.js';
 import authRoutes from './auth.route.js';
-import swaggerJSDoc from 'swagger-jsdoc';
-import { swaggerOptions } from '../../utils/swaggerOptions.js';
-import swaggerUi from 'swagger-ui-express';
-
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 export default express
   .Router()
   .get('/status', (req, res) => res.send('OK'))
   .use('/users', userRoutes)
-  .use('/auth', authRoutes)
-  .use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  .use('/auth', authRoutes);
